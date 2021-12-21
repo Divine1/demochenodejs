1c7f165941e82a5a3eb44a174b5729322897ac3a
Divine <dchelladurai@vmware.com>
2021-12-21 20:56:17 +0530

    updated

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.get("/getdata",(req,res)=>{
+    console.log("in /getdata ")
+    res.send({data : "in getdata"})
+
+})
 console.log("PORT ",PORT)
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`);
